c328b2816838f612f58a3c4ee1bfb7c7b1428230
zainalabidinhs <zainalabidin.tongas@gmail.com>
2020-08-28 16:44:58 +0700

    new

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react'
-import Home from './latihan1/Home'
 import Barang from './latihan2/Barang'
 
 function App() {
     return (
         <>
-            <Home />
             <Barang />
         </>
     )
--- src/latihan2/Barang.jsx
+++ src/latihan2/Barang.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Footer from '../component/Footer'
+import Header from '../component/Header'
 import product from './product'
 
 const Barang = () => {
     return <div>
+        <Header />
         <product />
+        <Footer />
     </div>
 }
 export default Barang
