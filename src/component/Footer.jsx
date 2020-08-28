902deedda03214e12fb6a509a597b40f07b0313d
zainalabidinhs <zainalabidin.tongas@gmail.com>
2020-08-28 16:20:26 +0700

    footer

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
     return (
         <>
-            <p>Copyright 2020 @zaiinhs.</p>
+            <p>Copyright 2020<b>@zaiinhs.</b></p>
         </>
     )
 }
